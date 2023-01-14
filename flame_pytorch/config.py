29715f81d0dfbcd1b524413da4a7094b45023393
okayenu <206762705+okayenu@users.noreply.github.com>
2023-01-13 21:44:34 -0500

    Add flame model path config option

--- flame_pytorch/config.py
+++ flame_pytorch/config.py
@@ -8,3 +8,13 @@ parser.add_argument(
     default="./model/generic_model.pkl",
     help="flame model path",
 )
+
+parser.add_argument(
+    "--static_landmark_embedding_path",
+    type=str,
+    default="./model/flame_static_embedding.pkl",
+    help="Static landmark embeddings path for FLAME",
+)
+
+parser.add_argument(
+    "--dynamic_landmark_embedding_path",
