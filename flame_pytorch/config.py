64a695d40e9340f7a3c02a9e32b3089b57ad97de
okayenu <206762705+okayenu@users.noreply.github.com>
2023-01-16 17:37:17 -0500

    Add static landmark embedding path

--- flame_pytorch/config.py
+++ flame_pytorch/config.py
@@ -18,3 +18,13 @@ parser.add_argument(
 
 parser.add_argument(
     "--dynamic_landmark_embedding_path",
+    type=str,
+    default="./model/flame_dynamic_embedding.npy",
+    help="Dynamic contour embedding path for FLAME",
+)
+
+# FLAME hyper-parameters
+
+parser.add_argument(
+    "--shape_params", type=int, default=100, help="the number of shape parameters"
+)
