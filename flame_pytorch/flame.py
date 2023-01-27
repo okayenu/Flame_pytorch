1ab16c6877734062d7f6bc550c6e4f09a8cfd88b
okayenu <206762705+okayenu@users.noreply.github.com>
2023-01-27 16:05:35 -0500

    Add FLAME module header and imports

---
+++ flame_pytorch/flame.py
@@ -0,0 +1,10 @@
+"""
+FLAME Layer: Implementation of the 3D Statistical Face model in PyTorch
+
+It is designed in a way to directly plug in as a decoder layer in a
+Deep learning framework for training and testing
+
+It can also be used for 2D or 3D optimisation applications
+
+Author: Soubhik Sanyal
+Copyright (c) 2019, Soubhik Sanyal
