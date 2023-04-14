b278a63eb8196d75eec7c434f9b7ac887aae36e9
okayenu <206762705+okayenu@users.noreply.github.com>
2023-04-14 11:42:30 -0500

    Add setuptools packaging scaffold

---
+++ setup.py
@@ -0,0 +1,13 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# Basic information
+NAME = "FLAME_PyTorch"
+DESCRIPTION = "PyTorch implementation of the 3D FLAME model."
+VERSION = "0.0.1"
+AUTHOR = "Soubhik Sanyal"
+EMAIL = "soubhik.sanyal@tuebingen.mpg.de"
+LICENSE = "See LICENSE"
+REPOSITORY = "https://github.com/soubhiksanyal/FLAME_PyTorch"
