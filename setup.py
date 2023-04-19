0c8345f38600665915a747e375352a0c7e489d28
okayenu <206762705+okayenu@users.noreply.github.com>
2023-04-19 11:16:33 -0500

    Define package metadata and version

--- setup.py
+++ setup.py
@@ -11,3 +11,16 @@ AUTHOR = "Soubhik Sanyal"
 EMAIL = "soubhik.sanyal@tuebingen.mpg.de"
 LICENSE = "See LICENSE"
 REPOSITORY = "https://github.com/soubhiksanyal/FLAME_PyTorch"
+PACKAGE = "flame_pytorch"
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+# Define the keywords
+KEYWORDS = ["FLAME", "3D Face Modelling"]
+
+CLASSIFIERS = [
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+]
+
