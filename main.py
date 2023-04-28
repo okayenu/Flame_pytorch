b1c293058a218f7928138380f1ffa8d0491aad34
okayenu <206762705+okayenu@users.noreply.github.com>
2023-04-28 10:21:01 -0500

    Start FLAME visualization demo script

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Demo code to load the FLAME Layer and visualise the 3D landmarks on the Face
+
+Author: Soubhik Sanyal
+Copyright (c) 2019, Soubhik Sanyal
+All rights reserved.
+
+Max-Planck-Gesellschaft zur Foerderung der Wissenschaften e.V. (MPG) is holder of all proprietary rights on this
