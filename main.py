51736b3f37eb5e74c4e994aee022008e67b0be80
okayenu <206762705+okayenu@users.noreply.github.com>
2023-05-17 16:29:55 -0500

    Visualize landmark spheres in pyrender viewer

--- main.py
+++ main.py
@@ -70,3 +70,20 @@ if config.optimize_eyeballpose and config.optimize_neckpose:
 
 # Visualize Landmarks
 # This visualises the static landmarks and the pose dependent dynamic landmarks used for RingNet project
+faces = flamelayer.faces
+for i in range(8):
+    vertices = vertice[i].detach().cpu().numpy().squeeze()
+    joints = landmark[i].detach().cpu().numpy().squeeze()
+    vertex_colors = np.ones([vertices.shape[0], 4]) * [0.3, 0.3, 0.3, 0.8]
+
+    tri_mesh = trimesh.Trimesh(vertices, faces, vertex_colors=vertex_colors)
+    mesh = pyrender.Mesh.from_trimesh(tri_mesh)
+    scene = pyrender.Scene()
+    scene.add(mesh)
+    sm = trimesh.creation.uv_sphere(radius=0.005)
+    sm.visual.vertex_colors = [0.9, 0.1, 0.1, 1.0]
+    tfs = np.tile(np.eye(4), (len(joints), 1, 1))
+    tfs[:, :3, 3] = joints
+    joints_pcl = pyrender.Mesh.from_trimesh(sm, poses=tfs)
+    scene.add(joints_pcl)
+    pyrender.Viewer(scene, use_raymond_lighting=True)
